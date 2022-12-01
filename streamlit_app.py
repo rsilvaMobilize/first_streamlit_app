e33cf42e2b3c53fa6c9bc679951eb1477e33d23b
rsilvaMobilize <119361820+rsilvaMobilize@users.noreply.github.com>
2022-12-01 17:27:03 -0500

    Update streamlit_app.py
    Menu added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title("My parents New Healthy Diner")
+streamlit.title('My parents New Healthy Diner')
+
+streamlit.header('Brekfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocet Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
